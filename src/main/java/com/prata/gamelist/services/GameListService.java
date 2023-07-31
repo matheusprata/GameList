a25aea9e970f0d73ad4a28f54178906d308662b7
Matheus Prata <matheusprata.ti@gmail.com>
2023-07-31 10:39:24 -0300

    implementação da lists service

---
+++ src/main/java/com/prata/gamelist/services/GameListService.java
@@ -0,0 +1,22 @@
+package com.prata.gamelist.services;
+
+import com.prata.gamelist.entities.GameList;
+import com.prata.gamelist.entities.GameListDTO;
+import com.prata.gamelist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(GameListDTO::new).toList();
+    }
+}
