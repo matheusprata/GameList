d6a32829772006a778b1443b858963ee084d6cbf
matheusprata <matheusprata.ti@gmail.com>
2023-07-30 22:27:37 -0300

    implementação do Service FindById buscando o gameDTO por id

--- src/main/java/com/prata/gamelist/services/GameService.java
+++ src/main/java/com/prata/gamelist/services/GameService.java
@@ -2,9 +2,12 @@ package com.prata.gamelist.services;
 
 import com.prata.gamelist.dto.GameMinDTO;
 import com.prata.gamelist.entities.Game;
+import com.prata.gamelist.entities.GameDTO;
 import com.prata.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -12,6 +15,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
