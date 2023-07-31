178a9cf58630e68f79d7a4c7b18f611192273e60
Matheus Prata <matheusprata.ti@gmail.com>
2023-07-31 11:37:50 -0300

    implementação da list buscando a categoria do game por id, teste do sql no banco h2 antes colocar no repository

--- src/main/java/com/prata/gamelist/dto/GameMinDTO.java
+++ src/main/java/com/prata/gamelist/dto/GameMinDTO.java
@@ -1,21 +1,31 @@
 package com.prata.gamelist.dto;
 
 import com.prata.gamelist.entities.Game;
+import com.prata.gamelist.projections.GameMinProjection;
 
 public class GameMinDTO {
     private Long id;
     private String title;
     private Integer year;
-    private String platforms;
+    private String imgUrl;
     private String shortDescription;
 
     public GameMinDTO(Game entity) {
         id = entity.getId();
         title = entity.getTitle();
         year = entity.getYear();
-        platforms = entity.getPlatforms();
+        imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    }
+
     public GameMinDTO(){
     }
     public Long getId() {
@@ -27,8 +37,8 @@ public class GameMinDTO {
     public Integer getYear() {
         return year;
     }
-    public String getPlatforms() {
-        return platforms;
+    public String getImgUrl() {
+        return imgUrl;
     }
     public String getShortDescription() {
         return shortDescription;
