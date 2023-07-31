e154d2e6dd8741240c2d5e3d9241e8850c299be7
Matheus Prata <matheusprata.ti@gmail.com>
2023-07-31 09:51:04 -0300

    implementação no controller do get por id

--- src/main/java/com/prata/gamelist/controllers/GameController.java
+++ src/main/java/com/prata/gamelist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.prata.gamelist.controllers;
 
 import com.prata.gamelist.dto.GameMinDTO;
+import com.prata.gamelist.entities.GameDTO;
 import com.prata.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
