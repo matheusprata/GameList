ca994a0d3b18ef66d2d640fe6925dd39685129e7
Matheus Prata <matheusprata.ti@gmail.com>
2023-08-02 11:56:52 -0300

    implementação no service do movimento de posicao do jogo

--- src/main/java/com/prata/gamelist/services/GameListService.java
+++ src/main/java/com/prata/gamelist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.prata.gamelist.services;
 
 import com.prata.gamelist.entities.GameList;
 import com.prata.gamelist.entities.GameListDTO;
+import com.prata.gamelist.projections.GameMinProjection;
 import com.prata.gamelist.repositories.GameListRepository;
+import com.prata.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +16,24 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public void move (Long listId, int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+        for (int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId , list.get(i).getId(), i);
+        }
     }
 }
