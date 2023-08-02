528e10852a5818b1f6dbb96bea3d51927d21ae1a
Matheus Prata <matheusprata.ti@gmail.com>
2023-08-02 15:52:48 -0300

    implementação do post no controller e criação do replacement para receber a posição no post e fazer a alteração da fila de games

--- src/main/java/com/prata/gamelist/controllers/GameListController.java
+++ src/main/java/com/prata/gamelist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.prata.gamelist.controllers;
 
 import com.prata.gamelist.dto.GameMinDTO;
 import com.prata.gamelist.dto.GameListDTO;
+import com.prata.gamelist.dto.ReplacementDTO;
 import com.prata.gamelist.services.GameListService;
 import com.prata.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,10 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId){
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+      gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
---
+++ src/main/java/com/prata/gamelist/dto/ReplacementDTO.java
@@ -0,0 +1,19 @@
+package com.prata.gamelist.dto;
+
+public class ReplacementDTO {
+    private Integer sourceIndex;
+    private Integer destinationIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+    public Integer getDestinationIndex() {
+        return destinationIndex;
+    }
+    public void setDestinationIndex(Integer destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+}
